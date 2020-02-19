a01f8be000fd4857b9be3193fd4a946f875417e1
Adilson <adilsondc1@gmail.com>
2020-02-19 16:36:33 -0300

    ajustando tela

--- src/app/pages/usuario/usuario-cadastro/usuario-cadastro.component.ts
+++ src/app/pages/usuario/usuario-cadastro/usuario-cadastro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, AfterViewInit, ViewChildren, ElementRef } from '@angular/core';
-import { FormControlName, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControlName, FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Usuario } from 'src/app/core/modelos/usuario.model';
 import { Sexo } from 'src/app/core/modelos/sexo.model';
 import { ValidationMessages } from 'src/app/core/modelos/generic-form-validation';
@@ -23,18 +23,19 @@ export class UsuarioCadastroComponent implements OnInit, AfterViewInit {
   constructor(private fb: FormBuilder) { }
 
   ngAfterViewInit(): void {
-    throw new Error("Method not implemented.");
+    
   }
 
   
 
   ngOnInit() {
     this.cadastroForm = this.fb.group({
-      nome: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(200)]],
-      dataNascimento: ['', Validators.required],
+      nome: new FormControl(''),
+      dataNascimento: '',
       email: [''],
-      senha:[''],
-      sexo: ['']
+      senha: [''],
+      sexo:[''],
+      ativo:[]
     });
   }
 
--- src/app/pages/usuario/usuario.module.ts
+++ src/app/pages/usuario/usuario.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioCadastroComponent } from './usuario-cadastro/usuario-cadastro.component';
 import { UsuarioRoutingModule } from './usuario-routing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UsuarioCadastroComponent],
   imports: [
     CommonModule,
     UsuarioRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class UsuarioModule { }
